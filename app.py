8cef2ce0abece493397b7383e09a772ca41bf9c8
afshin-taherojarood <afshin.taherojarood.github@gmail.com>
2026-06-04 19:04:38 +0330

    add admin authentication

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from database import init_db, seed_products
+
+# ساخت دیتابیس اگه وجود نداشت
+init_db()
+seed_products()
 
 st.set_page_config(
     page_title="فروشگاه هوشمند",
